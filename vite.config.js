fef78329a304ae1762fc996e55c8286711e605d2
mohamed-nageh <mnageh308@gmail.com>
2025-07-22 11:26:23 +0300

    fix: set base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/Fresh-Cart/',  // Set the base to the GitHub Pages path
+// });
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/Fresh-Cart/',  // Set the base to the GitHub Pages path
-});
+  base: '/', 
+})
